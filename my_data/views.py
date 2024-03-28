af9129cd21829799ab19eb1ab0ebe5498914b028
Rajeev <rajeevlakshmanan003@gmail.com>
2024-03-28 15:46:25 +0530

    portfolio project completed.

---
+++ my_data/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def education(request):
+    return render(request,'education.html')
+
+def resume(request):
+    return render(request,'resume.html')
